57ab7cabdecc7e0a3f1e3b04f8b96a93d95838ff
Jossy Benitez <jossybr94@gmail.com>
2023-06-06 15:42:26 -0500

    Avanzando en formulario

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   return (
     <Fragment>
       <Header titulo="Clima React App" />
+     
 
       <div className="contenedor-form">
         <div className="container">
--- src/componentes/Header/Header.js
+++ src/componentes/Header/Header.js
@@ -1,15 +1,23 @@
 import React from "react";
 
-
 //El codigo utilizado para CSS es de materialize
 const Header = ({ titulo }) => {
   return (
     <nav>
-      <div className="nav-wrapper light-blue darken-2">
-        <a href="#!" className="brand-logo">{titulo}</a>
+      <div >
+        <a href="#!" lassName="nav-wrapper light-blue darken-2" >
+          {titulo}
+        </a>
       </div>
     </nav>
   );
 };
 
 export default Header;
+
+
+{/* <div className="nav-wrapper light-blue darken-2">
+        <a href="#!" className="brand-logo">
+          {titulo}
+        </a>
+      </div> */}
